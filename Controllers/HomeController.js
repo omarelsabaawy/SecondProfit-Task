b032c914399ccd555c30cee759efa8801a10f333
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-10-15 13:07:45 +0200

    added redis

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,16 +1,36 @@
 const axios = require('axios');
+const { createClient } = require('redis');
 
-// Fetch data from the Restcountries API
 const fetchCountryData = async () => {
-    try {
-        const response = await axios.get('https://restcountries.com/v3/all');
-        return response.data.map((country) => ({
-            name: country.name.common,
-            flagUrl: country.flags[0],
-        }));
-    } catch (error) {
-        console.error('Error fetching country data:', error);
-        return [];
+
+    const client = await createClient({
+        host: '127.0.0.1',
+        port: 6379,
+    })
+        .on('error', err => console.log('Redis Client Error', err))
+        .connect();
+
+    const countryData = await client.get('countryData');
+    await client.disconnect();
+
+    if (countryData) {
+        return JSON.parse(countryData);
+    } else {
+        try {
+            const response = await axios.get('https://restcountries.com/v3/all');
+            const countryData = response.data.map((country) => ({
+                name: country.name.common,
+                flagUrl: country.flags[0],
+            }));
+
+            await client.set('countryData', JSON.stringify(countryData));
+            await client.disconnect();
+
+            return countryData;
+        } catch (error) {
+            console.error('Error fetching or caching country data:', error);
+            return [];
+        }
     }
 };
 
